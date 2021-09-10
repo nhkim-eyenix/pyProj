e2b778fa41e763065703a5a45e6042c8403544c3
nhkim-eyenix <nhkim.eyenix@gmail.com>
2021-09-11 07:41:05 +0900

    Add 1_create_frame.py

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()   # initialize; essential function
+
+# set the size of the screen
+screen_width = 480 # horizontal size
+screen_height = 640 # vertical size
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# set title
+pygame.display.set_caption("Nado_game")
+
+# event loop
+running = True # Is the game running?
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+#exit pygame
+pygame.quit()
