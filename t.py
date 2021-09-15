c4a1364e88546dd599cd2f83f86a13f2572eb132
nhkim-eyenix <nhkim@eyenix.com>
2021-09-15 11:23:51 +0900

    clean files

--- t.py
+++
@@ -1,6 +0,0 @@
-prime = [3, 7, 11]
-print(prime)
-
-prime.append(13)
-
-print(prime)
--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import cv2
-
-image = cv2.imread("1.jpg", cv2.IMREAD_UNCHANGED)
-cv2.imshow("Leo", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
