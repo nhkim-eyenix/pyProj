99ebcd759d3ec8dd32226f0b239ae678dbd247b0
nhkim-eyenix <nhkim.eyenix@gmail.com>
2021-09-25 15:34:51 +0900

    st 0925

---
+++ practice/st210925.py
@@ -0,0 +1,10 @@
+# 2021-09-25 Sat
+# Source 13.py
+
+# ord(): To get char code value of the character
+
+ch = input('Input one character ')
+if len(ch) != 0:
+    ch = ch[0]
+    chv = ord(ch)
+    print('char: %s \t code value: %d [%s]' %(ch, chv, hex(chv)))
