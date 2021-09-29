73fa5d384817425ddc645aeacfc5026a3bc2fc9d
nhkim-eyenix <nhkim@eyenix.com>
2021-09-29 12:29:21 +0900

    update before studying

---
+++ practice/ppt_GridDistanceCheck.py
@@ -0,0 +1,7 @@
+# Check GridDistance per cm at PowerPoint
+# To get the value of the GridDistance with '? ActivePresentation.GridDistance on VBA Editor'
+# 2021-09-27
+oneCm = 28.34644
+for i in range(1, 6):
+    print(i * oneCm)
+
