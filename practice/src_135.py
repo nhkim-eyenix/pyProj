133b89ed364bb7abdbdf490ab3c3e63b2b510aab
nhkim-eyenix <nhkim.eyenix@gmail.com>
2021-10-04 11:08:00 +0900

    src 137, 138, 139

--- practice/src_135.py
+++ practice/src_135.py
@@ -7,6 +7,9 @@ add = lambda x, y: x+y
 ret = add(1, 3)
 print(ret)
 
+sub = lambda x, y: x - y
+print(sub(5, 3))
+
 # 2
 funcs = [lambda x: x+'.pptx', lambda x: x+'.docx']
 ret1 = funcs[0]('Intro')
@@ -14,4 +17,9 @@ ret2 = funcs[1]('Report')
 print(ret1)
 print(ret2)
 
-#3
+# 3
+names = {'Mary': 10999, 'Sams': 2111, 'Aimy': 9778, 'Tom': 20245, 'Michale': 27115, 'Bob': 5887, 'Kelly': 7855}
+print(names)
+print('after sorting...')
+ret3 = sorted(names.items(), key = lambda x: x[1])
+print(ret3)
